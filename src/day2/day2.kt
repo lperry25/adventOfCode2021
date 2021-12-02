8f11bbe10cbb1ed7c20c462b774e08ac0180ed28
Laura Perry <10005215+lperry25@users.noreply.github.com>
2021-12-02 08:00:02 +0100

    completed part 2

--- src/day2/day2.kt
+++ src/day2/day2.kt
@@ -30,8 +30,27 @@ fun part1(input: List<String>): Array<Int> {
     return coordinates
 }
 
-fun part2(input: List<String>) {
-
+fun part2(input: List<String>): Int {
+    var coordinates: Array<Int> = arrayOf(0, 0, 0)
+    for (directionLine in input){
+        var commandSplit: List<String> = directionLine.split(" ")
+        var direction: String = commandSplit[0]
+        var value: Int = commandSplit[1].toInt()
+        if (direction.equals("forward")){
+            // move forward
+            coordinates[0] = coordinates[0] + value
+            // increase depth by aim multiplied by x
+            coordinates[1] = coordinates[1] + (coordinates[2] * value)
+        } else if (direction.equals("down")){
+            // increase aim by x
+            coordinates[2] = coordinates[2] + value
+        } else if (direction.equals("up")){
+            // decareas aim by x
+            coordinates[2] = coordinates[2] - value
+        }
+    }
+    println("horizontal  ${coordinates[0]} depth ${coordinates[1]} aim ${coordinates[2]}")
+    return coordinates[0] * coordinates[1]
 }
 
 fun main(){
@@ -39,6 +58,7 @@ fun main(){
     //var input: List<String> = readFileToList("testInput.txt")
     var part1Result = part1(input)
     println("horizontal  ${part1Result[0]} depth ${part1Result[1]}")
-    println("product ${part1Result[0] * part1Result[1]}")
-   // var part2Result = part2(input)
+    println("part 1 - product ${part1Result[0] * part1Result[1]}")
+    var part2Result = part2(input)
+    println("part 2 - ${part2Result}")
 }
