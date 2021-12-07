327ee2f8856c2c1c2c27ce65a96175870a33a9be
Laura Perry <10005215+lperry25@users.noreply.github.com>
2021-12-07 10:01:14 -0700

    did not solve day 6 part 2

--- src/day6/day6.kt
+++ src/day6/day6.kt
@@ -5,48 +5,54 @@ import FileReader.readEntireFileContents
 val newFishLife = 8
 val oldFishReset = 6
 
-class FishLife(timer: Int){
-    var timer = timer
+fun newlySpawnFishChildren(days:Int): Int{
+    println("new fish days ${days - newFishLife}")
+    return 1 + ((days - newFishLife)/ oldFishReset)
+}
 
-    fun spanNewFish(): Boolean{
-        if (timer > 0){
-            // countdown the timer
-            timer -= 1
-            return false
-        }
-        // span a new fish and reset timer to initial count
-        timer = oldFishReset
-        return true
+fun existingFishChildren(initialLife: Int, days: Int): Int{
+    println("days - initialLife ${days-initialLife} oldfish reset $oldFishReset ${(days-initialLife)/ oldFishReset}")
+    return 1 + ((days - initialLife) / oldFishReset)
+}
+
+fun remainingDays(daysCounter: Int, roundCounter: Int): Int{
+    return daysCounter - (newFishLife * roundCounter)
+}
+
+fun spawnEntireFishFamily(initialLife: Int, days: Int): Int{
+    val parentFish = 1
+    val initialFishChildren = existingFishChildren(initialLife, days)
+    println("initial fish children $initialFishChildren for initial life of $initialLife and days $days")
+    var dayCount = days
+    var roundCount = 1
+    var fishFamily = 0
+    while (remainingDays(dayCount, roundCount) >= newFishLife){
+        fishFamily += newlySpawnFishChildren(remainingDays(dayCount, roundCount))
+        roundCount++
     }
+    println("fish family $fishFamily")
+    return parentFish + initialFishChildren + fishFamily
 }
 
 fun countFishForDays(fileName: String, days: Int): Int {
     //get the largest number in the list
     val initialFish: List<Int> = readEntireFileContents(fileName).split(',').map{it.toInt()}
+    var numOfInitialFish = initialFish.size
 
-    var totalFish: MutableList<FishLife> = initialFish.map{life -> FishLife(life)}.toMutableList()
-    var day = 1
-    while (day <= days){
-        println("day $day")
-        var newTotalFish: MutableList<FishLife> = emptyList<FishLife>().toMutableList()
-        totalFish.map{ fish ->
-            if (fish.spanNewFish()) {
-                newTotalFish.add(fish)
-                newTotalFish.add(FishLife(newFishLife))
-            } else{
-                newTotalFish.add(fish)
-            }
-        }
-        totalFish = newTotalFish
-        day++
+    println("we start with $numOfInitialFish fish $initialFish")
+
+    var totalFish = 0
+    for (fishLife in initialFish){
+        totalFish += spawnEntireFishFamily(fishLife, days)
     }
-    return totalFish.size
+    return totalFish
 }
 
 fun main(){
     var fileName = "src/day6/testInput.txt"
     //var fileName = "src/day6/myInput.txt"
 
-    println("Part 1: There are ${countFishForDays(fileName, 80)} after 80 days")
+    val totalDays = 18
+    println("Part 1: There are ${countFishForDays(fileName, totalDays)} after $totalDays days")
     //println("Part 2: There are ${setUpDiagram(fileName, true)} sections with more than 2 overlapping lines")
 }
